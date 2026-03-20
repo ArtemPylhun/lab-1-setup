f9cf95bc989ea55da51ca94e5e2c50243591bb0f
ArtemPylhun <artem.pylhun@oa.edu.ua>
2026-03-20 22:10:26 +0200

    added basePath for GitHub pages to see styles

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "lab-1-setup",
+
   /* config options here */
   reactCompiler: true,
 };
